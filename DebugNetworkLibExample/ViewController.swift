c6480fefd0197288fbdafdf700508b30e874bfeb
Phung Anh Dung <dung.pa@neo-lab.vn>
2020-11-14 13:51:32 +0700

    UPDATE EXAMPLE

--- DebugNetworkLibExample/ViewController.swift
+++ DebugNetworkLibExample/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        webView.load(URLRequest(url: URL(string: "https://google.com")!))
     }
 
 
